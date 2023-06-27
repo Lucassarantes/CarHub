8597de9b468a79659da8cd5cab97aa1c1f19b568
Lucas Salles <lucas.sarantes@gmail.com>
2023-06-27 17:36:28 -0300

    First Commit

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars() {
+  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+  const options = {
+	method: 'GET',
+	headers: {
+	  'X-RapidAPI-Key': 'f88f42b5cdmsh88f9d698b9cdc50p1afebajsn73094dfd13d7',
+	  'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+  };
+  const response = await fetch(url, options);
+	const result = await response.json();
+	return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+	const basePricePerDay = 50; // Base rental price per day in dollars
+	const mileageFactor = 0.1; // Additional rate per mile driven
+	const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+	// Calculate additional rate based on mileage and age
+	const mileageRate = city_mpg * mileageFactor;
+	const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+	// Calculate total rental rate per day
+	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+	return rentalRatePerDay.toFixed(0);
+  };	
