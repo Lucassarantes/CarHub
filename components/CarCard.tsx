c28119a47d7ead6d019de407b1b91db7a19827d4
Lucas Salles <lucas.sarantes@gmail.com>
2023-07-17 17:29:29 -0300

    Complete project

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
     car: CarProps;
@@ -35,7 +36,7 @@ const CarCard = ({ car }: CarCardProps) => {
       
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
@@ -88,6 +89,12 @@ const CarCard = ({ car }: CarCardProps) => {
             />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => {setIsOpen(false); console.log('isOpen');}}
+        car={car}
+      />
     </div>
   )
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,12 +2,38 @@
 
 import { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('');
+const SearchButton = ({ otherClasses }: { otherClasses :string }) => (
+  <button
+    type="submit"
+    className={`-ml-3 z-10 ${otherClasses}`}
+  >
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+)
 
-    const handleSearch = () => {
+const SearchBar = ({ setManufacturer, setModel }) => {
+    const [searchManufacturer, setSearchManufacturer] = useState('');
+    const [searchModel, setSearchModel] = useState("")
+    const router = useRouter();
 
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if(searchManufacturer === '' && searchModel === '') {
+        return alert('Please fill in the search bar');
+      }
+
+      setModel(searchModel);
+      setManufacturer(searchManufacturer);
     }
   
     return (
@@ -17,12 +43,32 @@ const SearchBar = () => {
     >
       <div className="searchbar__item">
         <SearchManufacturer
-          manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          selected={searchManufacturer}
+          setSelected={setSearchManufacturer}
         />
+        <SearchButton  otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car searchModel"
+        />
+        <input
+          type="text"
+          name="searchModel"
+          value={searchModel}
+          onChange={(e) => setSearchModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton  otherClasses="sm:hidden" />
+      </div>
+      <SearchButton  otherClasses="max-sm:hidden" />
     </form>
   )
 }
 
-export default SearchBar
+export default SearchBar;
